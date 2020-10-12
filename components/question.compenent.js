9d82d532fabf2403e375d5fc1f56181d603d24a8
AbdelRahman MassriAttal <abed_attal@hotmail.com>
2020-10-12 19:16:53 +0300

    Update question.compenent.js

--- components/question.compenent.js
+++ components/question.compenent.js
@@ -1,24 +1,19 @@
-import * as React from 'react';
+import * as React from "react";
 import Option from "./option.component.js";
-import { Text, View, StyleSheet, Image } from 'react-native';
+import { View, StyleSheet, Image,Text } from "react-native";
+import RadioGroup from 'react-native-radio-buttons-group';
 
 
-const Question = ({ question, options,setSelectedOption }) => {
-  return (
-    <View
-      style={{
-        flexDirection: "column",
-        height: 100,
-        padding: 20,
-      }}
-       >
+const Question = ({ question, options, setSelectedOption}) => {
+  let selectedButton = options.find(e => e.selected == true);
+  selectedButton = selectedButton ? selectedButton.value : false
+  return ( 
+    <View >
       <Text>{question}</Text>
-      
 
-      <Option options={options} setOption={(option)=>setSelectedOption(option)} />
-   
-      <View style={{margin:5}}/>
-        
+      <RadioGroup selected={false} radioButtons={options} onPress={setSelectedOption} />
+
+      <View style={{ margin: 5 }} />
     </View>
   );
 };
